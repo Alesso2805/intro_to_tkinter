43bfb65b8f99840bd161612a00c4a0923219de23
alesso2805 <alessandrodp2222@gmail.com>
2025-02-27 20:53:16 -0500

    worked with button and input commands

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# import tkinter
-# import turtle
-#
-# window = tkinter.Tk()
-# window.title("My First GUI Program")
-# window.minsize(width=500, height=300)
-#
-# my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-# my_label.pack(side="top")
-#
-# tim = turtle.Turtle()
-# tim.write("I am a Turtle", font=("Arial", 24, "bold"))
-#
-# while True:
-#     window.mainloop()
+import tkinter
+from tkinter import Entry
 
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+input = Entry()
+input.pack()
+print(input.get())
+
+
+
+
+
+
+
+while True:
+    window.mainloop()
--- playground.py
+++ playground.py
@@ -2,3 +2,26 @@ def add(*args):
     print(sum(args))
 
 add(1, 2, 3, 4, 5,6,7,8,9,10)
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(f"{key}: {value}")
+    n += kwargs["add"]
+    print(n)
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=1, multiply=2, divide=3, subtract=4)
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="Nissan", model="GT-R", color="Blue", seats=2)
+print(my_car.make)
+print(my_car.model)
