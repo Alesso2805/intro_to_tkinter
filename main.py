053d14f15080bcbd3c160c16dafb5384bb90bab0
alesso2805 <alessandrodp2222@gmail.com>
2025-03-12 10:59:15 -0500

    learned how to pack and grid with tkinter

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import tkinter
 from tkinter import Entry
 
-window = tkinter.Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
-
 def button_clicked():
     print("I got clicked")
     new_text = input.get()
     my_label.config(text=new_text)
 
-button = tkinter.Button(text="Click Me", command=button_clicked)
-button.pack()
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
-input = Entry()
-input.pack()
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+
+button = tkinter.Button(text="Button", command=button_clicked)
+button.grid(column=1, row=1)
+
+button = tkinter.Button(text="Button", command=button_clicked)
+button.grid(column=2, row=0)
+
+input = Entry(width=10)
 print(input.get())
-
-
-
+input.grid(column=3, row=4)
 
 
 
